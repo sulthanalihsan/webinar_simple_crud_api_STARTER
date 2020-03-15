97d04f3e19e9cb3b6c337ef63df067b4709392d7
Muhammad Sulthan Al Ihsan <sulthanalihsan5@gmail.com>
2020-03-15 18:44:07 +0700

    first commit

---
+++ lib/service/api_service.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:starter/model/response_barang_model.dart';
+import 'package:starter/model/response_post_model.dart';
+
+class ApiService {
+
+}
